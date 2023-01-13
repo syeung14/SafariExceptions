adb4abfd106aa43b748ff3131c37dd953ddea396
simon <simon@dancingcloudservices.com>
2023-01-13 12:51:49 -0700

    Break 3

--- src/main/java/either/Either.java
+++ src/main/java/either/Either.java
@@ -7,6 +7,7 @@ import java.nio.file.Path;
 import java.util.Map;
 import java.util.function.Consumer;
 import java.util.function.Function;
+import java.util.function.UnaryOperator;
 import java.util.stream.Stream;
 
 interface ExFunction<A, R> {
@@ -78,17 +79,60 @@ public class Either<S, F> {
     }
     return this;
   }
+
+//  public Either<S, F> mapIfFailure(UnaryOperator<Either<S, F>> op) {
+  public Either<S, F> recover(UnaryOperator<Either<S, F>> op) {
+    if (isFailure()) {
+      return op.apply(this);
+    } else {
+      return this;
+    }
+  }
+
+  public static <S, F> UnaryOperator<Either<S, F>> retries(
+      UnaryOperator<Either<S, F>> op, int limit) {
+    return e -> {
+      int triesLeft = limit;
+      while (e.isFailure() && triesLeft-- > 0) {
+        e = op.apply(e);
+      }
+      return e;
+    };
+  }
+
+  public static <S, F> UnaryOperator<Either<S, F>> retrySequence(
+      UnaryOperator<Either<S, F>> ... op) {
+    return e -> {
+      int idx = 0;
+      while (e.isFailure() && idx < op.length) {
+        e = op[idx++].apply(e);
+      }
+      return e;
+    };
+  }
 }
 
 
 class UseEither {
   private static Map<String, String> backup = Map.of(
-      "b.txt", "d.txt"
+      "b.txt", "d.txt",
+      "d.txt", "e.txt"
   );
+
+  private static Either<Stream<String>, Throwable> retry(
+      Either<Stream<String>, Throwable> e) {
+
+    String fn = e.getFailure().getMessage();
+    System.out.println(Colors.PURPLE
+        + "retrying: " + fn + Colors.RESET);
+    return ExFunction.wrap((String f) -> Files.lines(Path.of(f)))
+        .apply(fn);
+  }
 
   private static Either<Stream<String>, Throwable> tryBackup(
       Either<Stream<String>, Throwable> e) {
-    if (e.isFailure()) {
+// now suitable only for "mapIfFailure" or whatever I rename it to
+//    if (e.isFailure()) {
       String failedName = e.getFailure().getMessage();
       String backupName = backup.get(failedName);
       System.out.println(Colors.PURPLE
@@ -96,7 +140,15 @@ class UseEither {
           + " with " + backupName + Colors.RESET);
       e = ExFunction.wrap((String f) -> Files.lines(Path.of(f)))
           .apply(backupName);
-    }
+//    }
+    return e;
+  }
+
+  private static Either<Stream<String>, Throwable> pause(
+      Either<Stream<String>, Throwable> e) {
+    try {
+      Thread.sleep(3000);
+    } catch (InterruptedException ie) {} // ignore, not really ideal!
     return e;
   }
 
@@ -105,7 +157,19 @@ class UseEither {
         .map(ExFunction.wrap(fn -> Files.lines(Path.of(fn))))
         .map(e -> e.report(f -> System.out.println(
             Colors.PURPLE + "File not found: " + f.getMessage() + Colors.RESET)))
-        .map(UseEither::tryBackup)
+//        .map(e -> e.mapIfFailure(UseEither::tryBackup))
+
+//        .map(Either.retries(e -> e.recover(UseEither::tryBackup), 3))
+
+//        .map(e -> e.recover(UseEither::pause))
+//        .map(e -> e.recover(UseEither::retry))
+
+        .map(Either.retrySequence(
+            UseEither::pause,
+            UseEither::retry,
+            Either.retries(UseEither::tryBackup, 3)
+            ))
+
         .filter(Either::isSuccess)
         .flatMap(Either::get)
         .forEach(System.out::println);
